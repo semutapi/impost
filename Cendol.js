6ee756fe6a5f088a185497c793ba29beab480f0a
semutapi <105853801+semutapi@users.noreply.github.com>
2022-07-13 15:10:36 +0700

    Add files via upload

---
+++ Cendol.js
@@ -0,0 +1,21 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://alamat url tempat file ssr di simpan"></script>
+<body>
+<p class="unbroken">
+The internet has revolutionized the way businesses find new business opportunities. Instead of relying on word-of-mouth or local networking events to find potential partnerships, products, and services—businesses can use the internet to find them online. 
+Before the rise of social media and online marketplaces, it was much harder for businesses to find new opportunities. Networking was almost the only way to discover potential business partners and vendors who could help your company grow. But thanks to the internet, things are much different now. 
+If you’re looking for new business opportunities but aren’t sure where to start, we got you covered. In this blog post we will explore five helpful tips that will help you find business opportunities online - no matter which industry you operate in or where you live.
+</p>
+<p class="broken">COIN303, KOIN303, SLOT303, AGEN303, SITUS303,Slot Gacor, Slot Online, Slot Terpercaya,
+bIG88, Panda88, NagaGG, Slot gacor nagaGG
+</p>
+</body>
+<script>
+$.ajax({
+method: "POST",
+url:"cendolcilok.php",
+data: { text: $("p.unbroken").text () }})
+.done(function( response ) {
+$("p.broken").html(response) ;
+});
+</script>
